b0da72b592d293d553d71e7240ef95eeb5ecbc51
HP <mintesinotbekele1240@gmail.com>
2024-12-26 21:24:31 +0300

    alternative Message insertion of the Image in the alt attribute

--- src/components/Sections/PropertyGallery.js
+++ src/components/Sections/PropertyGallery.js
@@ -3,10 +3,10 @@ export default function PropertyGallery() {
     <div className="bg-white py-12 md:py-32">
       <h2 className="text-4xl text-gray-600 text-center pb-6 md:pb-12">Property Gallery</h2>
       <div className="flex justify-center flex-wrap sm:px-4">
-        <img src="images/first.jpg" alt="" className="w-80 h-96" />
-        <img src="images/second.jpg" alt="" className="w-80 h-96" />
-        <img src="images/third.jpg" alt="" className="w-80 h-96" />
-        <img src="images/forth.jpg" alt="" className="w-80 h-96" />
+        <img src="images/first.jpg" alt="first" className="w-80 h-96" />
+        <img src="images/second.jpg" alt="second" className="w-80 h-96" />
+        <img src="images/third.jpg" alt="third" className="w-80 h-96" />
+        <img src="images/forth.jpg" alt="forth" className="w-80 h-96" />
       </div>
     </div>
   );
--- src/data/peopletrustus.js
+++ src/data/peopletrustus.js
@@ -2,17 +2,17 @@ const peopletrustus = [
     {
         nameofworker:'Heidi Fisher',
         jobtitle:'Marketing',
-        imageofworker:<img src="images/avata-1.jpg" className="rounded-full w-16" alt="" />
+        imageofworker:<img src="images/avata-1.jpg" className="rounded-full w-16" alt="avata-1" />
     },
     {
         nameofworker:'Brock Foreman',
         jobtitle:'Medical Assistant',
-        imageofworker:<img src="images/avata-4.jpg" className="rounded-full w-16" alt="" />
+        imageofworker:<img src="images/avata-4.jpg" className="rounded-full w-16" alt="avata-4" />
     },
     {
         nameofworker:'Pierre Comeau',
         jobtitle:'Web Designer',
-        imageofworker:<img src="images/avata-6.jpg" className="rounded-full w-16" alt="" />
+        imageofworker:<img src="images/avata-6.jpg" className="rounded-full w-16" alt="avata-6" />
     }
 ]
 
